b6b13d48d5046793a8d76692cd1e2708107d8c01
Dan Miller <dan.miller@york.ac.uk>
2020-07-20 11:48:17 +0100

    Fix invalid parameter
    Swap PARAMETER_FILE for PARAMETER_OVERRIDES

--- entrypoint.sh
+++ entrypoint.sh
@@ -72,5 +72,5 @@ echo "[default]
 output = text
 region = $AWS_REGION" > ~/.aws/config
 
-array=(aws cloudformation deploy --stack-name $AWS_STACK_NAME --template-file $TEMPLATE $PARAMETER_FILE $CAPABILITIES $ROLE_ARN $FORCE_UPLOAD --no-fail-on-empty-changeset)
+array=(aws cloudformation deploy --stack-name $AWS_STACK_NAME --template-file $TEMPLATE $PARAMETER_OVERRIDES $CAPABILITIES $ROLE_ARN $FORCE_UPLOAD --no-fail-on-empty-changeset)
 eval $(echo ${array[@]})
